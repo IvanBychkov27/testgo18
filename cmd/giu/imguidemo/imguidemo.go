9f3b720353706b2635267f274d24ea175202ebad
Ivan <IvanBychkov@mail.ru>
2022-04-26 19:54:19 +0300

    imgui.ShowDemoWindow

---
+++ cmd/giu/imguidemo/imguidemo.go
@@ -0,0 +1,17 @@
+// Показать демонстрационное окно создает демонстрационное/тестовое окно.
+// Демонстрирует большинство функций ImGui.
+package main
+
+import (
+	g "github.com/AllenDang/giu"
+	"github.com/AllenDang/imgui-go"
+)
+
+func loop() {
+	imgui.ShowDemoWindow(nil)
+}
+
+func main() {
+	wnd := g.NewMasterWindow("Widgets", 1024, 768, 0)
+	wnd.Run(loop)
+}
