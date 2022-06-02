ec6275e38312422453e955729c73c6b10faa92f8
Ivan <IvanBychkov@mail.ru>
2022-06-02 14:38:57 +0300

    Добавлено обучение нейросети с произвольным количеством входных, скрытых и выходных нейронов

--- cmd/neuralnet/nn_unit/nn_unit.go
+++ cmd/neuralnet/nn_unit/nn_unit.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"time"
 )
 
 type Data struct {
 	x []float64 // входные данных
-	r float64   // результат
+	r []float64 // результат
 }
 
 // нейрон
@@ -18,7 +19,8 @@ type Unit struct {
 }
 
 type NN struct {
-	nn [][]*Unit
+	nn  [][]*Unit // данные нейросети
+	mse float64   // среднеквадратичная ошибка нейросети
 }
 
 // создание нейрона:
@@ -53,42 +55,184 @@ func newNN(input, hidden, output int) *NN {
 
 	nn = append(nn, inputLayer, hiddenLayer, outputLayer)
 
-	return &NN{nn}
+	return &NN{nn, 1}
+}
+
+func getData0() []Data {
+	data := []Data{
+		{[]float64{1, 0, 0}, []float64{0.0}},
+		{[]float64{1, 1, 0}, []float64{0.3}},
+		{[]float64{1, 0, 1}, []float64{0.6}},
+		{[]float64{1, 1, 1}, []float64{1.0}},
+	}
+	return data
 }
 
 func getData() []Data {
 	data := []Data{
-		{[]float64{1, 0, 0}, 0.0},
-		{[]float64{1, 1, 0}, 0.1},
-		{[]float64{1, 0, 1}, 0.2},
-		{[]float64{1, 1, 1}, 0.3},
+		{[]float64{1, 0, 0, 0, 0}, []float64{0.0}},
+		{[]float64{1, 1, 0, 0, 0}, []float64{0.1}},
+		{[]float64{1, 0, 1, 0, 0}, []float64{0.2}},
+		{[]float64{1, 1, 1, 0, 0}, []float64{0.3}},
+		{[]float64{1, 0, 0, 1, 0}, []float64{0.4}},
+		{[]float64{1, 1, 0, 1, 0}, []float64{0.5}},
+		{[]float64{1, 0, 1, 1, 0}, []float64{0.6}},
+		{[]float64{1, 1, 1, 1, 0}, []float64{0.7}},
+		{[]float64{1, 0, 0, 0, 1}, []float64{0.8}},
+		{[]float64{1, 1, 0, 0, 1}, []float64{0.9}},
+		{[]float64{1, 0, 1, 0, 1}, []float64{1.0}},
+	}
+	return data
+}
+
+func getData2() []Data {
+	data := []Data{
+		{[]float64{1, 0, 0, 0, 0}, []float64{0.00}},
+		{[]float64{1, 1, 0, 0, 0}, []float64{0.08}},
+		{[]float64{1, 0, 1, 0, 0}, []float64{0.16}},
+		{[]float64{1, 1, 1, 0, 0}, []float64{0.24}},
+		{[]float64{1, 0, 0, 1, 0}, []float64{0.32}},
+		{[]float64{1, 1, 0, 1, 0}, []float64{0.40}},
+		{[]float64{1, 0, 1, 1, 0}, []float64{0.48}},
+		{[]float64{1, 1, 1, 1, 0}, []float64{0.56}},
+		{[]float64{1, 0, 0, 0, 1}, []float64{0.64}},
+		{[]float64{1, 1, 0, 0, 1}, []float64{0.72}},
+		{[]float64{1, 0, 1, 0, 1}, []float64{0.80}},
+		{[]float64{1, 1, 1, 0, 1}, []float64{0.88}},
+		{[]float64{1, 0, 0, 1, 1}, []float64{0.96}},
+		{[]float64{1, 1, 0, 1, 1}, []float64{1.0}},
 	}
 	return data
 }
 
 func main() {
-	//rand.Seed(time.Now().UnixNano())
-	nn := newNN(3, 2, 1)
+	rand.Seed(time.Now().UnixNano())
+	nn := newNN(5, 4, 1)
 	data := getData()
 
-	//submitEntryNN(nn, data[0].x) // подаем данные на входной слой нейросети
-	res := directWorkNN(nn, data[0].x)
+	trainingNN(nn, data)
+	calculateAllErrRateNN(nn, data, true)
 
-	fmt.Printf("res = %2.4f\n", res)
-	printNN(nn)
+	//idx := 1
+	//d := data[idx].x
+	//r := data[idx].r
+	//res := directWorkNN(nn, d)
+	//fmt.Printf("exp = %2.4f  res = %2.4f  mse:%0.4f\n", r, res, MSE(res, r))
+	//printNN(nn)
+}
+
+func trainingNN(net *NN, ds []Data) {
+	rate := 0.5
+	mseMin := 1.0
+	era := 0
+	for {
+		era++
+		for _, d := range ds { // перебор всех входных данных
+			submitEntryNN(net, d.x)      // подаем данные на входной слой нейросети
+			us := directWorkNN(net, d.x) // текущий результат нейросети
+			for i, u := range us {       // перебор всех резултатов нейросети
+				errNN := u - d.r[i]                // отклонение от ожидаемого результата
+				dw := errNN * sigmoidDerivative(u) // дельта весов для слоя outputLayer
+				reverseDistribution(net, dw, rate) // корректировка весов нейросети
+			}
+		}
+
+		if era%5 == 0 {
+			errRate := calculateAllErrRateNN(net, ds, false)
+			if mseMin > errRate {
+				mseMin = errRate
+			} else {
+				break
+			}
+
+			//fmt.Println("mse:", errRate, era)
+			if era > 1000000 {
+				break
+			}
+
+			if rate > 0.01 {
+				rate -= 0.01
+			} else {
+				rate -= 0.001
+			}
+
+			if rate <= 0 {
+				rate = 0.0001
+			}
+		}
+
+	}
+	net.mse = mseMin
+	fmt.Println("era :", era)
+	//fmt.Println("rate:", rate)
+}
+
+// метод обратного распределение ошибки - корректировка весов нейросети
+func reverseDistribution(net *NN, dw, rate float64) {
+	const (
+		inputLayer  = 0
+		hiddenLayer = 1
+		outputLayer = 2
+	)
+	var nn *Unit
+
+	// корректировка весов слоя outputLayer
+	for l := 0; l < len(net.nn[outputLayer]); l++ { // перебор всех эл-тов слоя outputLayer
+		nn = net.nn[outputLayer][l]
+		for i := 0; i < len(net.nn[hiddenLayer]); i++ {
+			nn.w[i] -= net.nn[hiddenLayer][i].value * dw * rate // корректировка весов слоя outputLayer
+		}
+	}
+
+	// корректировка весов слоя hiddenLayer
+	for l := 0; l < len(net.nn[hiddenLayer]); l++ { // перебор всех эл-тов слоя hiddenLayer
+		nn = net.nn[hiddenLayer][l]
+
+		var errHL float64                               // средняее отклонение от ожидаемого результата для слоя hiddenLayer
+		for t := 0; t < len(net.nn[outputLayer]); t++ { // перебор всех эл-тов слоя outputLayer
+			errHL += net.nn[outputLayer][t].w[l] * dw //
+		}
+		errHL = errHL / float64(len(net.nn[outputLayer]))
+
+		dwHL := errHL * sigmoidDerivative(nn.value) // дельта весов для слоя hiddenLayer
+		for i := 0; i < len(net.nn[inputLayer]); i++ {
+			nn.w[i] -= net.nn[inputLayer][i].value * dwHL * rate // корректировка весов слоя hiddenLayer
+		}
+	}
+}
+
+// расчет общей среднеквадратичной погрешности нейросети
+// print = true - выводит погрешности всех входных данных на экран
+func calculateAllErrRateNN(net *NN, ds []Data, print bool) float64 {
+	var errRateNN float64 // погрешность нейросети
+
+	if print {
+		fmt.Println("exp      res      mse")
+	}
+
+	for _, d := range ds {
+		y := directWorkNN(net, d.x)
+		mse := MSE(y, d.r)
+		errRateNN += mse
+		if print {
+			fmt.Printf("%0.2f   %0.2f   %0.6f\n", d.r, y, mse)
+		}
+	}
+
+	if print {
+		fmt.Printf("total mse: %0.6f\n", errRateNN/float64(len(ds)))
+	}
+
+	return errRateNN / float64(len(ds))
 }
 
 // прямая работа нейросети:
 // data - входные данные,
 // nn - нейросеть
 func directWorkNN(net *NN, data []float64) []float64 {
-	const (
-		inputLayer = iota
-		hiddenLayer
-		outputLayer
-	)
 	var dataLayer []float64
-	for i := hiddenLayer; i < len(net.nn); i++ {
+	submitEntryNN(net, data)           // подаем данные на входной слой нейросети
+	for i := 1; i < len(net.nn); i++ { // начинаем со скрытого слоя
 		dataLayer = make([]float64, 0, len(net.nn[i]))
 		for _, d := range net.nn[i] {
 			v := unitCalc(data, d.w)
@@ -136,8 +280,15 @@ func sigmoidDerivative(x float64) float64 {
 }
 
 // среднеквадратичная ошибка
-func MSE(x, r float64) float64 {
-	return math.Pow((r - x), 2.0)
+func MSE(actuals, expected []float64) float64 {
+	if len(actuals) != len(expected) {
+		return -1
+	}
+	var sum float64
+	for i, actual := range actuals {
+		sum += math.Pow((expected[i] - actual), 2.0)
+	}
+	return sum / float64(len(actuals))
 }
 
 // распечатать данные нейросети
